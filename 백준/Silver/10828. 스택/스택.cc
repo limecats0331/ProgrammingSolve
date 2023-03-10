d55964a74b4cde2a7448539cb300820dcca325f7
MinjaeKIm <limecats0331@gmail.com>
2023-03-10 17:55:42 +0900

    [Silver IV] Title: 스택, Time: 328 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    stack<int> stack1;
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        string cmd;
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int num;
+            cin >> num;
+            stack1.push(num);
+        } else if (cmd == "pop") {
+            if (stack1.empty()) {
+                cout << -1 << "\n";
+            } else {
+                cout << stack1.top() << "\n";
+                stack1.pop();
+            }
+        } else if (cmd == "size") {
+            cout << stack1.size() << "\n";
+        } else if (cmd == "empty") {
+            if (stack1.empty()) {
+                cout << 1 << "\n";
+            } else {
+                cout << 0 << "\n";
+            }
+        } else if (cmd == "top") {
+            if (stack1.empty()) {
+                cout << -1 << "\n";
+            } else {
+                cout << stack1.top() << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
