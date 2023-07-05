e17d89de41e1bf2b9c24e5ef0c579c1190e69cde
MinjaeKim <limecats0331@gmail.com>
2023-07-05 16:05:34 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+const ll INF = 1e18;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    ll n1, n2;
+    cin >> n1 >> n2;
+    cout << abs(n1 - n2);
+
+    return 0;
+}
+
